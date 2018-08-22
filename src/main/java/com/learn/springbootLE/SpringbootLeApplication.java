a6b7406429f38b27476959e8b11a926532802b89
xzeng <806019582@qq.com>
2018-08-22 14:10:35 +0800

    "初始化项目"

---
+++ src/main/java/com/learn/springbootLE/SpringbootLeApplication.java
@@ -0,0 +1,12 @@
+package com.learn.springbootLE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLeApplication.class, args);
+	}
+}
